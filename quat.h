5419d6bc002ba1181b547020afd903cd5407530e
Matthew Peddie <peddie@alum.mit.edu>
2014-11-27 14:30:18 +1000

    Add comments in quaternion header

--- quat.h
+++ quat.h
@@ -29,10 +29,10 @@
 #ifndef QUAT_T
 #define QUAT_T
 typedef struct quat_t {
-  double q0;
-  double q1;
-  double q2;
-  double q3;
+  double q0;  /* real */
+  double q1;  /* i */
+  double q2;  /* j */
+  double q3;  /* k */
 } quat_t;
 #endif // QUAT_T
 
@@ -43,13 +43,52 @@ typedef struct quat_t {
 extern "C"{
 #endif
 
+  /* Set 'q1' to 'q0'. */
   void quat_memcpy( quat_t * q1, const quat_t * const q0);
+
+  /* Set 'q_out' to the inverse of 'q_in'. */
   void quat_inv(quat_t *q_out, const quat_t * const q_in);
+
+  /* Return the norm of 'q'. */
   double quat_norm(const quat_t * const q);
+
+  /* Normalize 'q' to unit magnitude. */
   void quat_normalize(quat_t * q);
+
+  /* Compose two quaternions 'qa' and 'qb', so that 'qb' happens
+   * before 'qa'.  This is analogous to the order that rotation
+   * matrices compose; if you want to rotate a vector v by matrix Ra
+   * and then by matrix Rb, you get
+   *
+   *   v' = Rb × Ra × v
+   *
+   * so similarly, if you want to rotate v by qa and then by qb, you
+   * get
+   *
+   *   v' = rot(qb × qa, v)
+   *
+   */
   void quat_mult(quat_t *q_out, const quat_t * const qa, const quat_t * const qb);
+
+  /* Compose two quaternions 'qa' and 'qb', so that the inverse of
+   * 'qb' happens before 'qa'; i.e.
+   *
+   * q_out = qa × qb^-1
+   *
+   */
   void quat_mult_inv(quat_t *q_out, const quat_t * const qa, const quat_t * const qb);
+
+  /* Compose two quaternions 'qa' and 'qb' so that 'qb' happens before
+   * the inverse of 'qa'; i.e.
+   *
+   * q_out = qa^-1 × qb
+   *
+   */
   void quat_inv_mult(quat_t *q_out, const quat_t * const qa, const quat_t * const qb);
+
+  /* Compose two quaternions 'qa' and 'qb' so that 'qb' happens before
+   * 'qa', and return the resulting composite by value on the
+   * stack. */
   quat_t quat_mult_ret(const quat_t qa, const quat_t qb);
 
 #ifdef __cplusplus
